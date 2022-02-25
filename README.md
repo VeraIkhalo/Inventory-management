c370f123ee7bb2ce1ca19f461ce760baa367e027
Vera Ikhalo <ikhalo.oseaghe@gmail.com>
2022-02-25 15:58:44 +0100

    Feat: add the edit and delete functionality

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# cloud inventory
+# books-app
 
 ## Project setup
 ```
--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,77 @@
 <template>
-  <v-app>
-  <Navbar/>
-    <v-content>
-      <router-view></router-view>
-    </v-content>
-  
-  </v-app>
- 
+  <div id="app">
+    <AddBookItem v-on:add-book-event="addBookItem" v-on:edit-book-event="editBookItemEvent" v-bind:editBook="editBook"/>
+    <div>
+      <Books v-bind:books="books" v-on:del-book-event="deleteBookItem" v-on:edit-book-event="editBookItem" />
+    </div>
+  </div>
 </template>
 
 <script>
+  import Books from "./components/Books";
+  import AddBookItem from "./components/AddBookItem";
 
-import Navbar from '@/components/Navbar'
 export default {
   name: 'App',
-
   components: {
-   Navbar,
+    Books,
+    AddBookItem
   },
-
-  data: () => ({
-    //
-  }),
-};
+  data () {
+    return {
+      books: [],
+      editBook: {
+        title: '',
+        id: ''
+      }
+    }
+  },
+  methods: {
+    addBookItem(newBook){
+      // console.log('newbook', newBook.title);
+        this.books = [...this.books, newBook];
+      // this.books.unshift(newBook)
+    },
+    deleteBookItem(id){
+      this.books = this.books.filter(book => book.id !== id);
+    },
+    editBookItem(id){
+      //find the index of the book's id
+      let objIndex = this.books.findIndex(obj=> obj.id === id);
+      this.editBook.title = this.books[objIndex].title;
+      this.editBook.id = id;
+    },
+    editBookItemEvent(bookItem){
+      //find the index of this id's object
+      let objIndex = this.books.findIndex(obj => obj.id === bookItem.id)
+      //update the item
+      this.books[objIndex].title = bookItem.title;
+    }
+  },
+  watch: {
+    books: {
+      handler() {
+        localStorage.setItem('books',JSON.stringify(this.books))
+      },
+      deep: true
+    }
+  },
+  mounted() {
+    if (localStorage.getItem("books")){
+      this.books = JSON.parse(localStorage.getItem("books"))
+    }
+  }
+}
 </script>
+
+<style>
+#app {
+  text-align: center;
+  background: white;
+  width:100%;
+  height:100vh;
+  color: #2c3e50;
+  padding:80px;
+  
+}
+</style>
